782f5c426bdcc71a119dafe47df047bb8dccd98a
Marcio H <marciohlo@yahoo.com.br>
2023-04-08 18:21:00 -0300

    prisma migration, shared routes landingpage route fix

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import unidadeMedida from "src/modules/UnidadeMedida/routes/unidademedida.routes";
-import landingPage from "src/modules/LangingPage/routes/landingPage.routes";
+import landingPage from "src/modules/LandingPage/routes/landingPage.routes";
 import documento from "src/modules/Documento/routes/documento.routes";
 import endereco from "src/modules/Endereco/routes/endereco.routes";
 import produto from "src/modules/Produto/routes/produto.routes";
